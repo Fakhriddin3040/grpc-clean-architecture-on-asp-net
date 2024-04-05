32239c58801985ba7bc8205a8304554d19da51a4
Fakhriddin <fahriddin3040@gmail.com>
2024-04-05 23:16:46 +0500

    ...
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- src/AuthMicroservice.Application/obj/Debug/net8.0/AuthMicroservice.Application.AssemblyInfo.cs
+++ src/AuthMicroservice.Application/obj/Debug/net8.0/AuthMicroservice.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthMicroservice.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c98af49f6c1e40ba5ca3b657e53adb577ace3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ad3cf3b34bd7b90f31f0f717b8627fc8bf49e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthMicroservice.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthMicroservice.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
